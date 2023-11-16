1bf9c0035cf5fcec9dd4df8d55dea5d411d5c29e
sakhawat71 <sakhawat.showrav@gmail.com>
2023-11-16 21:35:03 +0600

    create error page

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,8 +1,10 @@
 
 const ErrorPage = () => {
     return (
-        <div>
-            
+        <div className="text-center">
+            <h1 className="font-extralight text-3xl bg-red-600">404</h1>
+            <p className="text-xl">Page not found</p>
+            <button className="btn"></button>
         </div>
     );
 };
