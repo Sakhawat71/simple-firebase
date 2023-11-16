13a2afeb4785932281fa9da3ecf1f0dcb181c2a8
sakhawat71 <sakhawat.showrav@gmail.com>
2023-11-16 21:31:20 +0600

    tailwind and daisyUI

---
+++ src/Router/Router.jsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const Route = createBrowserRouter([
+    {
+        path: "/",
+        errorElement:,
+        element:,
+    }
+])
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBs0FkJm3Re4pt4l1QOkuQonb3vwktRWXI",
+  authDomain: "simple-firebase-sabbir71.firebaseapp.com",
+  projectId: "simple-firebase-sabbir71",
+  storageBucket: "simple-firebase-sabbir71.appspot.com",
+  messagingSenderId: "762734970271",
+  appId: "1:762734970271:web:786c5651856be52eb6a9aa"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
