199594f7d9d9e688da4f92b7926db37e97751e82
sakhawat71 <sakhawat.showrav@gmail.com>
2023-11-16 21:44:40 +0600

    create router and Root Home navbar

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,9 +1,20 @@
 import { createBrowserRouter } from "react-router-dom";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
+import Root from "../components/Root/Root";
+import Home from "../components/Home/Home";
 
-const Route = createBrowserRouter([
+const MyRoute = createBrowserRouter([
     {
         path: "/",
-        errorElement:,
-        element:,
+        errorElement: <ErrorPage></ErrorPage>,
+        element: <Root></Root>,
+        children: [
+            {
+                path: "/",
+                element: <Home></Home>
+            }
+        ]
     }
 ])
+
+export default MyRoute;
