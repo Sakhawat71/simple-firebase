ad7d5961d4417d433f1eee0334da2ff670d8b9b9
sakhawat71 <sakhawat.showrav@gmail.com>
2023-11-17 02:08:39 +0600

    get & show user data

--- src/components/LogIn/Login.jsx
+++ src/components/LogIn/Login.jsx
@@ -1,60 +1,53 @@
 
 import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 import app from "../../firebase/firebase.init";
+import { useState } from "react";
 
 
 
 const Login = () => {
 
+    const [user, setUser] = useState([]);
+
     const auth = getAuth(app);
     const porvider = new GoogleAuthProvider()
 
-    const handelGoogleSignIn  =()=>{
-        signInWithPopup(auth,porvider)
-        .then(result =>{
-            const user = result.user;
-            console.log(user)
-        })
-        .catch(error => {
-            console.log(error)
-        })
+    const handelGoogleSignIn = () => {
+        signInWithPopup(auth, porvider)
+            .then(result => {
+                const loggedUser = result.user;
+                // console.log(loggedUser)
+                setUser(loggedUser);
+            })
+            .catch(error => {
+                console.log('error', error)
+            })
     }
 
+    console.log(user)
 
-    
+
 
     return (
-        <div className="mt-20 flex justify-center">
+        <div>
+            <div className="mt-20 flex justify-center">
+                <button
+                    onClick={handelGoogleSignIn}
+                    className="btn"
+                >Sign with Google</button>
+            </div>
 
-            <button
-                onClick={handelGoogleSignIn}
-                className="btn"
-            >Sign with Google</button>
-
+            {
+                user && <div className="text-center my-10">
+                    <figure className="flex justify-center"><img src={user.photoURL} alt="" /></figure>
+                    <h2>{user.displayName}</h2>
+                    <h3>{user.email}</h3>
+                    <p>{user.metadata?.creationTime}</p>
+                </div>
+            }
         </div>
+
     );
 };
 
 export default Login;
-
-/**
- *     const auth = getAuth(app);
-    const provider = new GoogleAuthProvider();
-
-    const handelGoogleSignIn = () => {
-
-        signInWithPopup(auth, provider)
-            .then(result => {
-                const user = result.user;
-                console.log(user);
-
-            })
-            .catch(error => {
-                console.log('error', error)
-        })
-
-    }
- * 
- * 
- * 
- */
